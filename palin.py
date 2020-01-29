1daeba794b024bb48cb75efb6ecc877c414488e5
NITHISH2104 <60419081+NITHISH2104@users.noreply.github.com>
2020-01-29 10:52:18 +0530

    Add files via upload

---
+++ palin.py
@@ -0,0 +1,13 @@
+a="madam"
+for i in a:
+    print(i)
+    for j in range (4,0):
+        
+            if(i==j):
+            
+                print("palindrom")
+                
+            else:
+                print("not palindrom")
+                    
+    
